9ed854e24a08a06c04e98837a1c91cddcc175ad1
NikhilRohan <nikhilrohan0108@gmail.com>
2022-11-05 00:51:41 +0530

    Card addded made dynamic

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,26 @@ import "./App.css";
 import CardComponent from "./components/CardComponent/CardComponent";
 import HeroComponent from "./components/HeroComponent/HeroComponent";
 import NavBarComponent from "./components/NavbarComponent/NavBarComponent";
+import cardData from "/src/data.js";
 
 function App() {
+  const cardElements = cardData.map((eachCardData) => {
+    return (
+      <CardComponent
+        img={eachCardData.coverImg}
+        rating={eachCardData.stats.rating}
+        reviewCount={eachCardData.stats.reviewCount}
+        location={eachCardData.location}
+        title={eachCardData.title}
+        price={eachCardData.price}
+      />
+    );
+  });
   return (
     <div>
       <NavBarComponent />
       <HeroComponent />
-      <CardComponent />
+      <div className="cardComponentGroup">{cardElements}</div>
     </div>
   );
 }
--- src/components/CardComponent/CardComponent.jsx
+++ src/components/CardComponent/CardComponent.jsx
@@ -2,19 +2,19 @@ import "/src/components/CardComponent/CardComponent.css";
 import KatiePicture from "/src/assets/Katie-Picture.png";
 import Star from "/src/assets/Star.png";
 
-const CardComponent = () => {
+const CardComponent = (props) => {
   return (
     <div className="cardComponent">
-      <img src={KatiePicture} className="pictureSection" />
+      <img src={"/src/assets/" + props.img} className="pictureSection" />
       <div class="ratingSection">
         <img src={Star} className="starSection" />
-        <p className="ratings">5.0</p>
-        <p className="reviews">(6)</p>
-        <p className="country">. USA</p>
+        <p className="ratings">{props.rating}</p>
+        <p className="reviews">({props.reviewCount})</p>
+        <p className="country">{props.location}</p>
       </div>
-      <div className="cardTitle">Life lessons with Katie Zaferes</div>
+      <div className="cardTitle">{props.title}</div>
       <div className="cost">
-        <b>From $136</b> / person
+        <b>From ${props.price}</b> / person
       </div>
     </div>
   );
